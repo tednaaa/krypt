b9b2eb73eeb9b6edb96e31e3d00e446a753b0615
Andranik <tednaaa@gmail.com>
2025-12-23 15:02:55 +0400

    remove everything

--- src/indicators/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod ema;
-pub mod pivot;
-
-pub use ema::MultiEma;
-pub use pivot::PivotLevels;
--- src/pump_scanner/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod analysis;
-pub mod detector;
-pub mod tracker;
-
-pub use analysis::SignalAnalysis;
-pub use detector::{PumpCandidate, PumpDetector};
-pub use tracker::TrackerManager;
