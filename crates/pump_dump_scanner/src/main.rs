c27e7b14d4a035c7e8be840e5971d163b776847a
Andranik <tednaaa@gmail.com>
2025-12-23 19:50:17 +0400

    add logger

--- crates/pump_dump_scanner/src/main.rs
+++ crates/pump_dump_scanner/src/main.rs
@@ -1,9 +1,22 @@
 use exchanges::{BinanceExchange, Exchange};
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+	tracing_subscriber::fmt()
+		.with_env_filter(
+			tracing_subscriber::EnvFilter::try_from_default_env()
+				.unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+		)
+		.init();
+
+	info!("Starting pump/dump scanner");
+
 	let binance = BinanceExchange::new();
-	println!("{:?}", binance.get_funding_rate_info("MINA").await?);
+
+	let test_symbol = "ANIMEUSDT";
+	info!("{:?}", binance.get_funding_rate_info(test_symbol).await?);
+	info!("{:?}", binance.get_open_interest_info(test_symbol).await?);
 
 	Ok(())
 }
