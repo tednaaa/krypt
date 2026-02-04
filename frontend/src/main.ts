40e4804baafbbd3cd7b49e5b7932c990842d43a4
Andranik <tednaaa@gmail.com>
2026-02-05 01:50:05 +0400

    pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,21 @@
+import { PiniaColada } from '@pinia/colada';
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+
 import App from './App.vue';
 import { router } from './pages/router';
+
 import './assets/css/main.css';
 
-createApp(App)
-  .use(router)
-  .mount('#app');
+const app = createApp(App);
+
+app.use(router);
+
+app.use(createPinia());
+app.use(PiniaColada, {
+  queryOptions: {
+    staleTime: 60 * 1000, // 1 minute
+  },
+});
+
+app.mount('#app');
